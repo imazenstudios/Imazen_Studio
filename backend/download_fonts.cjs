1853c215539beb92a65f291198d407677b9b6437
saiprasanth03 <ssaiprasanth333@gmail.com>
2026-07-15 06:55:04 +0530

    fix: remove corrupt font files to restore PDF generation

--- backend/download_fonts.cjs
+++ backend/download_fonts.cjs
@@ -8,12 +8,40 @@ if (!fs.existsSync(fontsDir)) {
 }
 
 const fonts = {
-  'PlayfairDisplay-Bold.ttf': 'https://github.com/google/fonts/raw/main/ofl/playfairdisplay/PlayfairDisplay-Bold.ttf',
-  'PlayfairDisplay-Regular.ttf': 'https://github.com/google/fonts/raw/main/ofl/playfairdisplay/PlayfairDisplay-Regular.ttf',
-  'Oswald-Bold.ttf': 'https://github.com/google/fonts/raw/main/ofl/oswald/Oswald-Bold.ttf',
-  'Oswald-Regular.ttf': 'https://github.com/google/fonts/raw/main/ofl/oswald/Oswald-Regular.ttf',
-  'Raleway-Regular.ttf': 'https://github.com/google/fonts/raw/main/ofl/raleway/Raleway-Regular.ttf',
-  'Raleway-Bold.ttf': 'https://github.com/google/fonts/raw/main/ofl/raleway/Raleway-Bold.ttf'
+  'PlayfairDisplay-Bold.ttf': 'https://fonts.googleapis.com/css?family=Playfair+Display:700',
+  'PlayfairDisplay-Regular.ttf': 'https://fonts.googleapis.com/css?family=Playfair+Display:400',
+  'Oswald-Bold.ttf': 'https://fonts.googleapis.com/css?family=Oswald:700',
+  'Oswald-Regular.ttf': 'https://fonts.googleapis.com/css?family=Oswald:400',
+  'Raleway-Regular.ttf': 'https://fonts.googleapis.com/css?family=Raleway:400',
+  'Raleway-Bold.ttf': 'https://fonts.googleapis.com/css?family=Raleway:700'
+};
+
+const getTtfUrl = (cssUrl) => {
+  return new Promise((resolve, reject) => {
+    https.get(cssUrl, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko' // IE11 requests TTF/WOFF
+      }
+    }, res => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        // Find url(...) in the CSS
+        const match = data.match(/url\((https:\/\/[^)]+\.ttf)\)/);
+        if (match && match[1]) {
+          resolve(match[1]);
+        } else {
+          // fallback try to find any url
+          const fallbackMatch = data.match(/url\((https:\/\/[^)]+)\)/);
+          if (fallbackMatch) {
+             resolve(fallbackMatch[1]);
+          } else {
+             reject(new Error('No TTF URL found in CSS'));
+          }
+        }
+      });
+    }).on('error', reject);
+  });
 };
 
 const download = (filename, url) => {
@@ -21,18 +49,13 @@ const download = (filename, url) => {
     const filePath = path.join(fontsDir, filename);
     const file = fs.createWriteStream(filePath);
     https.get(url, response => {
-      if (response.statusCode === 302) {
-        https.get(response.headers.location, res => {
-          res.pipe(file);
-          file.on('finish', () => {
-            file.close(resolve);
-          });
-        }).on('error', reject);
-      } else {
+      if (response.statusCode === 200) {
         response.pipe(file);
         file.on('finish', () => {
           file.close(resolve);
         });
+      } else {
+        reject(new Error(`Failed to download ${url}, status: ${response.statusCode}`));
       }
     }).on('error', err => {
       fs.unlink(filePath, () => reject(err));
@@ -41,13 +64,16 @@ const download = (filename, url) => {
 };
 
 async function downloadAll() {
-  for (const [filename, url] of Object.entries(fonts)) {
-    console.log(`Downloading ${filename}...`);
+  for (const [filename, cssUrl] of Object.entries(fonts)) {
+    console.log(`Getting TTF URL for ${filename}...`);
     try {
-      await download(filename, url);
+      const ttfUrl = await getTtfUrl(cssUrl);
+      console.log(`Found TTF URL: ${ttfUrl}`);
+      console.log(`Downloading ${filename}...`);
+      await download(filename, ttfUrl);
       console.log(`Finished ${filename}`);
     } catch (e) {
-      console.error(`Error downloading ${filename}`, e);
+      console.error(`Error with ${filename}`, e);
     }
   }
 }
